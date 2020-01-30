06ebc2ae1a7d2d920e0f33d8a80748243bad55bb
Sherry Shi <shi.yu@wustl.edu>
2020-01-30 15:23:30 -0600

    step 3

--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -6,13 +6,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import android.view.View
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun launchSecondActivity(view: View) {
+        //Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, ChangeActivity::class.java)
+        startActivity(intent);
+
 
     }
 
